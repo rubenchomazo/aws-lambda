029e66656a1771f7997e670cbfc632122bfff226
Rubén Mazo <rubenmazo0403@gmail.com>
2021-07-21 18:49:12 -0500

    Prueba secrets app

--- src/test/java/com/aws/lambda/test/EmployeeRepositoryIntegrationTest.java
+++ src/test/java/com/aws/lambda/test/EmployeeRepositoryIntegrationTest.java
@@ -50,7 +50,6 @@ public class EmployeeRepositoryIntegrationTest {
     }
 
     @Test
-    @Ignore
     public void testSaveOrEmployee() {
         Employee employee = new Employee();
         employee.setId("003");
@@ -61,7 +60,6 @@ public class EmployeeRepositoryIntegrationTest {
 
 
     @Test
-    @Ignore
     public void testGetEmployees() {
         List<Employee> employeeList = (List<Employee>) repository.findAll();
         assertTrue(employeeList != null);
